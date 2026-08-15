41e90d815cba1025ce34d77cde588a474f444112
Mandla Mkhabela <mandlamkhabela3000@gmail.com>
2026-08-15 09:44:54 +0200

    feat: Update Yoco secret key handling for test mode

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -13,7 +13,7 @@
 const TEST_MODE = true;
 const YOCO_SECRET_KEY = TEST_MODE
   ? 'sk_test_118399e5bBbMmDZac2145cdb09ee'
-  : 'sk_live_7986a9f6bBbMmDZ3359478a87855';
+  : '';
 
 const ADMIN_SECRET = 'MoonRiver89Drifts#Up';
 const DEFAULT_PRICE_CENTS = 2500;
